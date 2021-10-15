1b0099743115988f0570049e31a246352b4ef4f8
Mavis Murdock <mavis@somewords.org>
2021-10-15 10:23:02 -0400

    make -r and -n optional but require at least one of them

--- logcheck.py
+++ logcheck.py
@@ -17,11 +17,17 @@ def loops(args):
 
 
 def service_providers(args):
-    # if a username has been specified, then deal with that in relying_parties
-    if args.name:
-        relying_parties(args)
-        if args.relying_party == []:
-            return 0
+
+    # -r is not specified
+    if args.relying_party == None:
+        # neither -r nor -n specified
+        if args.name == None:
+            print("You must specify either -n or -r or both.")
+        # -n specified
+        else:
+            relying_parties(args)
+        return 0
+
     for party in args.relying_party: # allows for multiple party ID arguments
         kwargs = {
             'idpv': args.idp_version,
@@ -32,21 +38,27 @@ def service_providers(args):
             log.load(filename)
         log.command_service_providers()
 
+    # both -n and -r switches specified
+    if not args.name == None:
+        args.relying_party = None
+        relying_parties(args)
+
+
 # works similarly to service_providers: for a given username, print out each relying_party accessed by it
 def relying_parties(args):
+
      for name in args.name:
             kwargs = {
                 'idpv': args.idp_version,
                 'name': name,
             }
-            if args.relying_party == []:
-                kwargs['relying_party'] = None # we'll get an error if there's an empty list
+
+            kwargs['relying_party'] = args.relying_party   # we'll get an error if there's an empty list
 
             log = ShibbolethLog(**kwargs)
             for filename in args.filename:
                 log.load(filename)
             log.command_relying_parties()
-
 
 if __name__ == '__main__':
     argp = ArgumentParser()
@@ -60,7 +72,7 @@ if __name__ == '__main__':
                       help='Log filename(s) to process, accepts wildcards')
     sp_p.add_argument('-i', '--idp-version', default=4,
                       help='IdP version')
-    sp_p.add_argument('-r', '--relying-party', required=True, default=None, nargs='*',
+    sp_p.add_argument('-r', '--relying-party', default=None, nargs='*',
                       help='Restrict to this relying party and list users')
     sp_p.add_argument('-n', '--name', default=None, nargs='*',
                       help='Restrict to this entity id and list relying parties') # takes a username and returns the relying party IDs (with counts)
