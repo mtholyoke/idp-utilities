7a6e00b105041b7edf89dc97ace6d933ff9d3560
Jae Proctor <jproctor@mtholyoke.edu>
2024-12-20 13:43:35 -0500

    Clean up the driver script a little

--- logscan.py
+++ logscan.py
@@ -3,24 +3,20 @@
 from argparse import ArgumentParser
 from parsers import ShibbolethLog
 import datetime as dt
-import re
 
-today = dt.date.today()
-first = today.replace(day=1)
-last_month = first - dt.timedelta(days=1)
-month_default = last_month.strftime("%Y-%m")
 
 def help(args):
     argp.print_help()
+
 
 def scan(args):
     kwargs = {
         'principal': args.principal,
         'requester': args.requester,
-        'daily' : args.daily,
-        'month' : args.month,
-        'output' : args.output,
-        'sso' : args.sso,
+        'sso': args.sso,
+        'daily': args.daily,
+        'output': args.output,
+        'month': None,
     }
     log = ShibbolethLog(**kwargs)
     for filename in args.filename:
@@ -30,22 +26,51 @@ def scan(args):
 
 if __name__ == '__main__':
     argp = ArgumentParser(
-        epilog='Specify neither -n nor -r to show all service providers. Specify both to see IP address and timestamp of logins.')
-    argp.add_argument('-f', '--filename', type=str, nargs='*',
-                      default=['/opt/shibboleth-idp/logs/idp-process.log'],
-                      help='Log filename(s) to process, accepts wildcards')
-    argp.add_argument('-n', '--principal', default=None, nargs='+',
-                      help='Restrict to this username and list service providers')
-    argp.add_argument('-r', '--requester', default=None, nargs='+',
-                      help='Restrict to this service provider and list usernames')
-    argp.add_argument('-d', '--daily', action='store_true',
-                      help='Provide daily output as CSV')
-    # argp.add_argument('-m', '--month', default=None, const=month_default, nargs='?',
-    #                   help='Restrict to this month and list service providers and usernames')
-    argp.add_argument('-o', '--output', default=None, nargs='?',
-                      help='Create logs of results in this output directory')
-    argp.add_argument('-s', '--sso', action='store_true',
-                      help='Determine if SSO was used within restrictions')
+        description='''
+            Scans Shib logs for instances of specified (or all) usernames
+            logging into specified (or all) service providers, and returns
+            counts as CSV of either totals or daily traffic.
+        ''',
+        epilog='''
+            Specify neither -n nor -r to show all usernames and service
+            providers. Specify both to see IP address and timestamp of
+            all logins.''',
+    )
+
+    subject = argp.add_argument_group('Subjects to scan for')
+    subject.add_argument(
+        '-n', '--principal', default=None, nargs='+',
+        help='Limit scan to the username(s) provided')
+    subject.add_argument(
+        '-r', '--requester', default=None, nargs='+',
+        help='Limit scan to the service provider(s) provided')
+    subject.add_argument(
+        '-s', '--sso', action='store_true',
+        help='Determine if SSO was used within above limits')
+
+    output = argp.add_argument_group('Output options')
+    # TODO: -d needs exactly one of -n or -r.
+    output.add_argument(
+        '-d', '--daily', action='store_true',
+        help='Provide daily output as CSV')
+    output.add_argument(
+        '-o', '--output', default=None, nargs='?',
+        help='Create logs of results in this output directory')
+    # output.add_argument(
+    #     '-v', '--verbose', action='store_true',
+    #     help='Provide verbose output')
+
+    targets = argp.add_argument_group('Which log files to scan')
+    # today = dt.date.today()
+    # last_month = today.replace(day=1) - dt.timedelta(days=1)
+    # month_default = last_month.strftime("%Y-%m")
+    # targets.add_argument(
+    #     '-m', '--month', default=None, const=month_default, nargs='?',
+    #     help='Use all log files from a specific month')
+    targets.add_argument(
+        '-f', '--filename', type=str, nargs='*',
+        default=['/opt/shibboleth-idp/logs/idp-process.log'],
+        help='Log filename(s) to process, accepts wildcards')
 
     args = argp.parse_args()
     scan(args)
