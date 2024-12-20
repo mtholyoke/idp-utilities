70c9103e892ae7b1177ea531b628a5178b06509f
Jae Proctor <jproctor@mtholyoke.edu>
2024-12-20 13:45:59 -0500

    Give up on throwing warnings about new Shib modules

--- parsers/shibboleth.py
+++ parsers/shibboleth.py
@@ -27,96 +27,8 @@ class ShibbolethLog(_LogFile):
     # Regex match groups:
     #     1: Username
     #     2: Status: one of 'succeeded', 'failed', 'produced exception'
-    LOGIN_REGEX = r"^Credential Validator ldap: Login by '?(.*)'? (\w+)$"
+    LOGIN_REGEX = r"^Credential Validator ldap: Login by '?(.*?)'? (.*)$"
 
-    # TODO: Some of these are probably useful events
-    SKIP_MODULES = [
-        'DEPRECATED',
-        'java.lang.IllegalStateException',
-        'net.shibboleth.idp.authn.impl.FinalizeAuthentication',
-        'net.shibboleth.idp.attribute.resolver.ad.impl.ContextDerivedAttributeDefinition',
-        'net.shibboleth.idp.attribute.resolver.impl.AttributeResolverImpl',
-        'net.shibboleth.idp.authn.AbstractUsernamePasswordCredentialValidator',
-        'net.shibboleth.idp.authn.ExternalAuthenticationException',
-        'net.shibboleth.idp.authn.impl.AttributeSourcedSubjectCanonicalization',
-        'net.shibboleth.idp.authn.impl.FilterFlowsByForcedAuthn',
-        'net.shibboleth.idp.authn.impl.SelectAuthenticationFlow',
-        'net.shibboleth.idp.profile.impl.SelectProfileConfiguration',
-        'net.shibboleth.idp.profile.impl.WebFlowMessageHandlerAdaptor',
-        'net.shibboleth.idp.saml.attribute.transcoding.impl.SAML2StringAttributeTranscoder',
-        'net.shibboleth.idp.saml.metadata.impl.ReloadingRelyingPartyMetadataProvider',
-        'net.shibboleth.idp.saml.saml2.profile.impl.PopulateEncryptionParameters',
-        'net.shibboleth.idp.saml.saml2.profile.impl.ProcessLogoutRequest',
-        'net.shibboleth.idp.saml.saml2.profile.impl.ValidateSAMLAuthentication',
-        'net.shibboleth.idp.session.impl.DetectIdentitySwitch',
-        'net.shibboleth.idp.session.impl.ProcessLogout',
-        'net.shibboleth.idp.session.impl.StorageBackedIdPSession',
-        'net.shibboleth.idp.ui.csrf.impl.CSRFTokenFlowExecutionListener',
-        'net.shibboleth.utilities.java.support.net.HttpServletSupport',
-        'net.shibboleth.utilities.java.support.security.DataSealer',
-        'net.shibboleth.utilities.java.support.security.impl.IPRangeAccessControl',
-        'org.apache.velocity.directive.parse',
-        'org.apache.velocity.loader',
-        'org.opensaml.profile.action.impl.DecodeMessage',
-        'org.opensaml.profile.action.impl.LogEvent',
-        'org.opensaml.saml.common.binding.impl.SAMLMetadataLookupHandler',
-        'org.opensaml.saml.common.binding.SAMLBindingSupport',
-        'org.opensaml.saml.common.binding.security.impl.MessageLifetimeSecurityHandler',
-        'org.opensaml.saml.common.binding.security.impl.MessageReplaySecurityHandler',
-        'org.opensaml.saml.common.binding.security.impl.ReceivedEndpointSecurityHandler',
-        'org.opensaml.saml.metadata.resolver.impl.AbstractMetadataResolver',
-        'org.opensaml.saml.saml2.binding.decoding.impl.HTTPRedirectDeflateDecoder',
-        'org.opensaml.security.crypto.SigningUtil',
-        'org.opensaml.storage.impl.client.ClientStorageService',
-        'org.opensaml.xmlsec.algorithm.AlgorithmSupport',
-        'org.opensaml.xmlsec.impl.BasicEncryptionParametersResolver',
-        'org.opensaml.xmlsec.keyinfo.impl.BasicProviderKeyInfoCredentialResolver',
-        'org.springframework.webflow.execution.FlowExecutionException',
-        'org.springframework.webflow.execution.repository.FlowExecutionRestorationFailureException',
-        'org.springframework.webflow.execution.repository.NoSuchFlowExecutionException',
-        'Shibboleth-Audit.Login',
-        'Shibboleth-Audit.Logout',
-        'Shibboleth-Audit.ResolverTest',
-        # These were on ion but not login:
-        'net.shibboleth.idp.attribute.resolver.AbstractResolverPlugin',
-        'net.shibboleth.idp.authn.impl.ValidateUsernamePasswordAgainstLDAP',
-        'net.shibboleth.idp.saml.attribute.mapping.AbstractSAMLAttributeMapper',
-        'net.shibboleth.idp.saml.attribute.mapping.AbstractSAMLAttributeValueMapper',
-        'net.shibboleth.idp.saml.nameid.impl.AttributeSourcedSAML2NameIDGenerator',
-        'net.shibboleth.idp.saml.profile.impl.PopulateBindingAndEndpointContexts',
-        'net.shibboleth.utilities.java.support.security.BasicKeystoreKeyStrategy',
-        'net.shibboleth.utilities.java.support.security.IPRangeAccessControl',
-        'net.shibboleth.utilities.java.support.xml.BasicParserPool',
-        'org.ldaptive.AbstractOperation$ReopenOperationExceptionHandler',
-        'org.opensaml.messaging.decoder.servlet.BaseHttpServletRequestXMLMessageDecoder',
-        'org.opensaml.saml.common.binding.security.impl.SAMLProtocolMessageXMLSignatureSecurityHandler',
-        'org.opensaml.saml.metadata.resolver.impl.AbstractReloadingMetadataResolver',
-        'org.opensaml.saml.metadata.resolver.impl.FileBackedHTTPMetadataResolver',
-        'org.opensaml.saml.metadata.resolver.impl.HTTPMetadataResolver',
-        'org.opensaml.saml.saml2.binding.decoding.impl.HTTPPostDecoder',
-        'org.opensaml.saml.saml2.profile.impl.AddNameIDToSubjects',
-        'org.springframework.webflow.execution.repository.BadlyFormattedFlowExecutionKeyException',
-        # These were on sso but not ion or login:
-        'java.lang.RuntimeException',
-        'net.shibboleth.idp.authn.AbstractUsernamePasswordValidationAction',
-        'net.shibboleth.idp.authn.impl.SelectSubjectCanonicalizationFlow',
-        'net.shibboleth.idp.profile.impl.ResolveAttributes',
-        'net.shibboleth.idp.saml.attribute.encoding.AbstractSAMLAttributeEncoder',
-        'net.shibboleth.idp.saml.attribute.encoding.impl.SAML2StringAttributeEncoder',
-        'net.shibboleth.idp.saml.attribute.mapping.AbstractSAMLAttributesMapper',
-        'net.shibboleth.idp.saml.nameid.impl.BaseTransientDecoder',
-        'net.shibboleth.idp.saml.nameid.impl.LegacyCanonicalization',
-        'org.ldaptive.pool.BlockingConnectionPool',
-        'org.opensaml.profile.action.impl.EncodeMessage',
-        'org.opensaml.saml.common.binding.security.impl.BaseSAMLSimpleSignatureSecurityHandler',
-        'org.opensaml.saml.saml2.binding.encoding.impl.HTTPPostEncoder',
-        'org.springframework.webflow.conversation.impl.LockTimeoutException',
-        'Shibboleth-Audit.anonymous',
-        'Shibboleth-Audit.AttributeQuery',
-        # These were only on idp5:
-        'net.shibboleth.ext.spring.error.ExtendedMappingExceptionResolver',
-        'net.shibboleth.idp.authn.PooledTemplateSearchDnResolver',
-    ]
     # Inherited variable:
     #     SEQUENCE_CLASS = _LogSequence
     # Inherited methods:
@@ -137,6 +49,8 @@ class ShibbolethLog(_LogFile):
             if login is None:
                 print('ERROR: can’t parse message', parse.string)
                 return None
+            if self.principal and login[1].lower() not in self.principal:
+                return None
             return ShibbolethEvent(
                 ip_addr=ip_addr,
                 time=time,
@@ -147,42 +61,30 @@ class ShibbolethLog(_LogFile):
             )
         if parse['module'] == 'Shibboleth-Audit.SSO':
             audit = parse['message'].split('|')
-            if self.events[-1].type == 'Login':
-                return ShibbolethEvent(
-                    ip_addr=ip_addr,
-                    time=time,
-                    level=level,
-                    type='Attribute',
-                    user=audit[3],
-                    entity_id=audit[4],
-                    attributes=audit[8],
-                    browser=audit[20],
-                    audit=audit,
-                    sso=False
+            if self.principal and audit[3].lower() not in self.principal:
+                return None
+            if self.requester and audit[4] not in self.requester:
+                return None
+            return ShibbolethEvent(
+                ip_addr=ip_addr,
+                time=time,
+                level=level,
+                type='Attribute',
+                user=audit[3].lower(),
+                entity_id=audit[4],
+                attributes=audit[8],
+                browser=audit[20],
+                audit=audit,
+                sso=(self.events[-1].type != 'Login'),
             )
-            else:
-                return ShibbolethEvent(
-                    ip_addr=ip_addr,
-                    time=time,
-                    level=level,
-                    type='Attribute',
-                    user=audit[3],
-                    entity_id=audit[4],
-                    attributes=audit[8],
-                    browser=audit[20],
-                    audit=audit,
-                    sso=True
-                )
-        print('Unknown log module:', parse['module'])
+        # print('Unknown log module:', parse['module'])
         return None
 
     def validate_line(self, parse):
-        if parse['module'] in self.SKIP_MODULES:
-            return False
         if parse['message'] == "Ignoring NameIDFormat metadata that includes the 'unspecified' format":
             return False
         return True
-    
+
     def process_like_link(self, target):
         if 'http' in target:
             if 'www.' in target:
@@ -195,11 +97,11 @@ class ShibbolethLog(_LogFile):
         return target
 
     """
-    Takes in a string array representing an amount of requests from a user for a service and if output directory is present, 
-    writes them into a csv file. If output is not present, then prints. 
+    Takes in a string array representing an amount of requests from a user for a service and if output directory is present,
+    writes them into a csv file. If output is not present, then prints.
     Parameters:
-        requests(string[]): will appear as user,#requests 
-        service(string): name of the service user is accessing, determines which file 
+        requests(string[]): will appear as user,#requests
+        service(string): name of the service user is accessing, determines which file
         the requests might go into
     Returns: None
     """
@@ -211,7 +113,7 @@ class ShibbolethLog(_LogFile):
                 sso_add = ""
             if self.month:
                 csvfile = open(f"./{self.output}/{service}_" + sso_add + f"{self.month}.csv", 'w')
-                log = csv.writer(csvfile, delimiter = ",") 
+                log = csv.writer(csvfile, delimiter = ",")
                 for request in requests:
                     log.writerow(request)
                 csvfile.close()
@@ -221,21 +123,20 @@ class ShibbolethLog(_LogFile):
                 log = csv.writer(csvfile, delimiter = ",")
                 for request in requests:
                     log.writerow(request)
-                csvfile.close() 
-            elif self.output: 
+                csvfile.close()
+            elif self.output:
                 csvfile = open(f"./{self.output}/{service}_" + sso_add + "all.csv", 'w')
                 log = csv.writer(csvfile, delimiter = ",")
                 for request in requests:
                     log.writerow(request)
-                csvfile.close() 
+                csvfile.close()
             else:
                 for request in requests:
                     print(request)
-    
+
     def command_scan(self):
         principal = self.principal is not None
         requester = self.requester is not None
-        month = self.month is not None
         output = self.output is not None
         daily = self.daily
         sso = self.sso
@@ -266,7 +167,7 @@ class ShibbolethLog(_LogFile):
             pass
 
         for event in self.events:
-                
+
             # Filter the events to the ones we care about.
             if event.type != "Attribute":
                 continue
@@ -281,9 +182,9 @@ class ShibbolethLog(_LogFile):
             test = not sso
             if to_count:
                 total += 1
-            
-            # Record what we want to keep track of.  
-            #print("to_count is " + str(to_count) + " when sso is " + str(sso) + " and event.sso is " + str(event.sso))                     
+
+            # Record what we want to keep track of.
+            #print("to_count is " + str(to_count) + " when sso is " + str(sso) + " and event.sso is " + str(event.sso))
             if principal and requester and to_count:
                 # Both -n and -r: print the detail.
                 print(
@@ -315,7 +216,7 @@ class ShibbolethLog(_LogFile):
                 # Neither -n nor -r nor -m: count visits to each site.
                 sites[event.entity_id] += 1
 
-        
+
         # Output the results.
         if daily:
             requests = []
@@ -333,9 +234,9 @@ class ShibbolethLog(_LogFile):
             self.show_output(requests, False)
         elif principal or requester or month:
             for target in sorted(report.keys()):
-                #put output in logs 
+                #put output in logs
                 service = self.process_like_link(target)
-                requests = []       
+                requests = []
                 for item, count in sorted(report[target].items(), key=lambda x: x[1], reverse=True):
                     user = target
                     site = item
