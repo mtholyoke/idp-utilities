f6679a20703ecdff2bc5d27c4d806d9f4f00d978
Jae Proctor <jproctor@mtholyoke.edu>
2026-04-01 11:00:55 -0400

    Create a reloader for attribute resolution

---
+++ reload.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from pathlib import Path
+import socket
+import ssl
+import urllib.parse
+import urllib.request
+import yaml
+
+
+def set_defaults(config={}):
+    if 'hostname' not in config:
+        config['hostname'] = socket.getfqdn()
+    return config
+
+
+if __name__ == '__main__':
+    script_dir = Path(__file__).resolve().parents[0]
+    ap = ArgumentParser()
+    ap.add_argument('--config', type=open,
+                    default=str(script_dir / 'config.yml'),
+                    help='YAML file with configuration options.')
+    ap.add_argument('-ar', '--attribute-resolver', action='append_const',
+                    const='AttributeResolverService', dest='services',
+                    help='Load changes from conf/attribute-resolver.xml')
+    args = ap.parse_args()
+    if args.services is None:
+        ap.print_help()
+        exit(1)
+
+    config = yaml.safe_load(args.config)
+    config = set_defaults(config)
+
+    base = f"https://{config['hostname']}/idp/profile/admin/reload-service"
+    for service in args.services:
+        query = {'id': f"shibboleth.{service}"}
+        url = f'{base}?{urllib.parse.urlencode(query)}'
+
+        cx = ssl.create_default_context()
+        cx.verify_mode = ssl.CERT_OPTIONAL
+        cx.check_hostname = False
+
+        response = urllib.request.urlopen(url, context=cx)
+        result = response.read().decode()
+        print(result)
