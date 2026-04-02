7c96e7bec17c8594512bdf7de00c5c38ebaf9f91
Becca Groveman <bgroveman@mtholyoke.edu>
2026-04-02 09:11:03 -0400

    add relying party reloading service

--- reload.py
+++ reload.py
@@ -32,6 +32,9 @@ if __name__ == '__main__':
     ap.add_argument('-m', '--metadata-resolver', action='append_const',
                     const='MetadataResolverService', dest='services',
                     help='Load changes from conf/metadata-providers.xml')
+    ap.add_argument('-r', '--relying-party-resolver', action='append_const',
+                    const='RelyingPartyResolverService', dest='services',
+                    help='Load changes from conf/relying-party.xml')
     args = ap.parse_args()
     if args.services is None:
         ap.print_help()
